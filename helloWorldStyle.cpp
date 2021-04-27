0d1128be1f6183203c9c6c72524957a544dba26a
Ubuntu <ubuntu@ip-172-31-93-33.ec2.internal>
2021-04-27 17:39:23 +0000

    this is the same as the pyhton version, it has one error using the styler, it has to do with tabs/spaces. Otherwise the code is perfect

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) Year Your Name All rights reserved.
+//
+// Created by: Liam Csiffary
+// Date: April 27, 2021
+// This program displays hello world but in the proper format
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello world!" << std::endl;
+}
